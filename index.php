bfc652e02334ef6b60393cd3e5c48353b12c204c
Alecpu92 <alessandro_belfiore@hotmail.it>
2023-06-22 10:24:31 +0200

    refactoring php password

--- index.php
+++ index.php
@@ -20,17 +20,29 @@
 </style>
 
 <?php
-     $letterMin = range('a' , 'z');
-     $letterCap = range('A' , 'Z');
+     $lettersMin = range('a' , 'z');
+     $lettersCap = range('A' , 'Z');
      $numbers = range(0,9);
      $special = [
         ...range("!" , "/"),
         ...range(":" , "@"),
         ...range("[" , "'"),
         ...range("{" , "-")
-     ]
+        ];
+
+        $final = [...$lettersMin, ...$lettersCap, ...$numbers, ...$special]
+
+
 
 $lng = $_GET['lenght'];
+
+$res = "";
+for ($x=0;$x<$lng;$x++) {
+
+    $rndInd = rand(0, count($final) - 1);
+    $rndChr = $final[$rndInd];
+    $res .= $rndChr;
+}
 
 ?>
 
